c6d9a405fad2a887badc2ab76b09ac12fabde2ef
developertariq <dev.tariqul.islam@gmail.com>
2023-01-11 10:58:08 +0600

    Change markup for calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -14,22 +14,22 @@ class Calculator extends React.Component {
           <button className="key" type="button">AC</button>
           <button className="key" type="button">+/-</button>
           <button className="key" type="button">%</button>
-          <button className="key orange" type="button">/</button>
+          <button className="key color-button" type="button">&#0247;</button>
           <button className="key" type="button">7</button>
           <button className="key" type="button">8</button>
           <button className="key" type="button">9</button>
-          <button className="key orange" type="button">*</button>
+          <button className="key color-button" type="button" style={{ fontSize: 15 }}>&#10006;</button>
           <button className="key" type="button">4</button>
           <button className="key" type="button">5</button>
           <button className="key" type="button">6</button>
-          <button className="key orange" type="button">-</button>
+          <button className="key color-button" type="button" style={{ fontSize: 30, fontWeight: 200 }}>-</button>
           <button className="key" type="button">1</button>
           <button className="key" type="button">2</button>
           <button className="key" type="button">3</button>
-          <button className="key orange" type="button">+</button>
-          <button className="key double" type="button">0</button>
+          <button className="key color-button" type="button">+</button>
+          <button className="key double-size" type="button">0</button>
           <button className="key" type="button">.</button>
-          <button className="key orange" type="button">=</button>
+          <button className="key color-button" type="button">=</button>
         </div>
       </div>
     );
