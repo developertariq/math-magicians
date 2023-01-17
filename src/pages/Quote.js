ce6d5acd8061ac3064a509d1e70a045862b6b838
developertariq <dev.tariqul.islam@gmail.com>
2023-01-18 02:13:32 +0600

    Add quote page

---
+++ src/pages/Quote.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Quote = () => (
+  <div>
+    <h2>
+      Mathematics is not about numbers, equations, computetions, or algorithms.
+      It is about understanding - William Paul Thurston
+    </h2>
+  </div>
+);
+
+export default Quote;
