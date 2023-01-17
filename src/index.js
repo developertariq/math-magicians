f55abaaea2c9800313708c8e07f43ccd095d5aa4
developertariq <dev.tariqul.islam@gmail.com>
2023-01-18 02:17:21 +0600

    Initialize react router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Home from './pages/Home';
+import Calculation from './pages/Calculation';
+import Quote from './pages/Quote';
+import Header from './components/Header';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+        </Route>
+        <Route path="/Calculator" element={<Calculation />} />
+        <Route path="/Quote" element={<Quote />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 );
-
-reportWebVitals();
