e2d63e43912e2a24d5fb697f1630ce797f244ed6
developertariq <dev.tariqul.islam@gmail.com>
2023-01-18 02:13:00 +0600

    Add hompepage

---
+++ src/pages/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <h2>
+      Welcome to our page!
+    </h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Veritatis placeat vero, ratione, molestiae modi obcaecati reprehenderit similique,
+      architecto asperiores dicta quod ducimus beatae quo
+      at voluptatibus possimus hic voluptas ab.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Veritatis placeat vero, ratione, molestiae modi obcaecati reprehenderit similique,
+      architecto asperiores dicta quod ducimus beatae
+      quo at voluptatibus possimus hic voluptas ab.
+    </p>
+  </div>
+);
+
+export default Home;
