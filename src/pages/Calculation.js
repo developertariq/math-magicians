116416608c834f7bd5c9fff602b3c2145a12210f
developertariq <dev.tariqul.islam@gmail.com>
2023-01-18 02:14:08 +0600

    Add calculator page

---
+++ src/pages/Calculation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Calculator from '../components/Calculator';
+
+const Calculation = () => (
+  <div>
+    <h2>
+      Let&apos;s do some math!
+    </h2>
+    <Calculator />
+  </div>
+);
+
+export default Calculation;
