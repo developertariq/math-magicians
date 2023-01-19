35935752df307e4aa9aacc50528adca27dc8a859
developertariq <dev.tariqul.islam@gmail.com>
2023-01-19 19:12:48 +0600

    Add files for update

---
+++ src/_test_/Quote.test.js
@@ -0,0 +1,11 @@
+// import React from 'react';
+// import renderer from 'react-test-renderer';
+// import '@testing-library/jest-dom';
+// import Quote from '../pages/Quote';
+
+// describe('Quote', () => {
+//   test('renders correctly', () => {
+//     const tree = renderer.create(<Quote />).toJSON();
+//     expect(tree).toMatchSnapshot();
+//   });
+// });
