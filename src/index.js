95aa5c77a570b53722f6a05b90e5609e812f6116
developertariq <dev.tariqul.islam@gmail.com>
2023-01-19 22:33:03 +0600

    Refactor for deploy

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App';
 import Home from './pages/Home';
 import Calculation from './pages/Calculation';
@@ -11,7 +11,7 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <Header />
       <Routes>
         <Route path="/" element={<App />}>
@@ -20,6 +20,6 @@ root.render(
         <Route path="/Calculator" element={<Calculation />} />
         <Route path="/Quote" element={<Quote />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>,
 );
