d3ed5a9c0bfda8f1f142d0a827c5d7a3d7284bce
Saba Ahmad <sabaahmad895@gmail.com>
2023-01-19 19:43:11 +0500

    cal test

--- src/_test_/Calculator.test.js
+++ src/_test_/Calculator.test.js
@@ -1,29 +1,29 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import { render } from "@testing-library/react";
-import { BrowserRouter as Router } from "react-router-dom";
-import "@testing-library/jest-dom";
-import Calculator from "../components/Calculator";
-import Calculation from "../pages/Calculation";
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import '@testing-library/jest-dom';
+import Calculator from '../components/Calculator';
+import Calculation from '../pages/Calculation';
 
-describe("Calculator page", () => {
-  test("renders Calculator page correctly", () => {
+describe('Calculator page', () => {
+  test('renders Calculator page correctly', () => {
     const tree = renderer.create(<Calculation />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
-  test("renders Calculator correctly", () => {
+  test('renders Calculator correctly', () => {
     const tree = renderer.create(<Calculator />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
-  test("renders the correct Calculator", () => {
+  test('renders the correct Calculator', () => {
     const { getByTestId } = render(
       <Router>
         <Calculator />
-      </Router>
+      </Router>,
     );
-    const calculator = getByTestId("calculator");
+    const calculator = getByTestId('calculator');
     expect(calculator).toBeInTheDocument();
   });
 });
--- src/_test_/Quote.test.js
+++ src/_test_/Quote.test.js
@@ -1,10 +1,10 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import "@testing-library/jest-dom";
-import Quote from "../pages/Quote";
+import React from 'react';
+import renderer from 'react-test-renderer';
+import '@testing-library/jest-dom';
+import Quote from '../pages/Quote';
 
-describe("Quote", () => {
-  test("renders correctly", () => {
+describe('Quote', () => {
+  test('renders correctly', () => {
     const tree = renderer.create(<Quote />).toJSON();
     expect(tree).toMatchSnapshot();
   });
